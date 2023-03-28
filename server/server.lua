dc2e60f274bed383afea27e1dd83e4d7724a464d
ASTUDIOS | Development <64874485+aqade@users.noreply.github.com>
2023-03-28 10:42:20 +0200

    Initial Upload

--- server/server.lua
+++ server/server.lua
@@ -1,1 +1,10 @@
+--------------------------------------
+--<!>-- ASTUDIOS | DEVELOPMENT --<!>--
+--------------------------------------
+print("^2[astudios-pocketbmx] ::^0 Started")
+print("^2[astudios-pocketbmx] ::^0 Developed by ASTUDIOS | DEVELOPMENT")
+local QBCore = exports['qb-core']:GetCoreObject()
 
+QBCore.Functions.CreateUseableItem(Config.BikeItem, function(source)
+    TriggerClientEvent('astudios-pocketbmx:client:use', source)
+end)
